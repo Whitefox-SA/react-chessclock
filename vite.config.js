0a7bae502514f9ab06c723ebcbeb498c7c292d70
Manuela Salazar <david.s356811@cesurformacion.com>
2024-03-04 09:36:05 +0100

    cambio de configuracion de vite para hacer build en carpeta docs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.join(__dirname, "docs"),
+  }},{
   plugins: [
     react(),
     tailwindcss('./tailwind.config.js'),
